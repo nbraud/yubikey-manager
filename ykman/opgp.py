630b49af11b4c19f528672e62940b996f051ae1b
Dag Heyman <dag@yubico.com>
2019-02-25 13:49:24 +0100

    opgp: rename pin argument to admin_pin

--- ykman/opgp.py
+++ ykman/opgp.py
@@ -190,14 +190,14 @@ class OpgpController(object):
         self.send_apdu(0, INS.PUT_DATA, 0, key_slot.touch_position(),
                        bytes(bytearray([mode, TOUCH_METHOD_BUTTON])))
 
-    def set_pin_retries(self, pw1_tries, pw2_tries, pw3_tries, pin):
+    def set_pin_retries(self, pw1_tries, pw2_tries, pw3_tries, admin_pin):
         if self.version < (1, 0, 7):  # For YubiKey NEO
             raise ValueError('Setting PIN retry counters requires version '
                              '1.0.7 or later.')
         if (4, 0, 0) <= self.version < (4, 3, 1):  # For YubiKey 4
             raise ValueError('Setting PIN retry counters requires version '
                              '4.3.1 or later.')
-        self._verify(PW3, pin)
+        self._verify(PW3, admin_pin)
         self.send_apdu(0, INS.SET_PIN_RETRIES, 0, 0,
                        bytes(bytearray([pw1_tries, pw2_tries, pw3_tries])))
 
